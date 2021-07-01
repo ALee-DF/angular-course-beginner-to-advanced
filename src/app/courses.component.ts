df7403eddcf3833136cb86625119672c5f7ee073
Alexander Lee <alex8usa8@gmail.com>
2021-07-01 14:03:55 -0700

    Displaying Data and Handling Events: Attribute Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,16 +4,16 @@ import { Component } from '@angular/core';
     selector: 'courses',
 
     template: `
-        <h2>{{ title }}</h2>
-        <h2 [textContent]="title"></h2>
-
-        <img src="{{ imageUrl }}" />
-        <img [src]="imageUrl" />
         <img [src]=imageUrl />
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan"></td>
+            </tr>
+        </table>
     `
 })
 export class CoursesComponent {
-    title = "List of courses";
     imageUrl = "http://lorempixel.com/400/200/";
+    colSpan = 2;
     
 }
