fe0195f27fe4fb748f883e1c7400605277e3f609
Alexander Lee <alex8usa8@gmail.com>
2021-07-01 11:06:25 -0700

    Angular Fundamentals: Directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,10 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>'
+
+    // Whenever you are using a directive that modifies the structure of the DOM by adding or removing an element,
+    // you need to prefix "ngFor" with an asterisks (*)
+    template: `
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
     title = "List of courses";
+    courses = ["course1", "course2", "course3"]
 
     getTitle() {
         return this.title;
