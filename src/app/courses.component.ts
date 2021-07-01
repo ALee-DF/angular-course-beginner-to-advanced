e645a673c7440c03f9a4f4a2926fc119a8267dd1
Alexander Lee <alex8usa8@gmail.com>
2021-07-01 13:55:29 -0700

    Displaying Data and Handling Events: Property Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,33 +1,19 @@
 import { Component } from '@angular/core';
-import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
 
-    // Whenever you are using a directive that modifies the structure of the DOM by adding or removing an element,
-    // you need to prefix "ngFor" with an asterisks (*)
     template: `
-        <h2>{{ getTitle() }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{ course }}
-            </li>
-        </ul>
+        <h2>{{ title }}</h2>
+        <h2 [textContent]="title"></h2>
+
+        <img src="{{ imageUrl }}" />
+        <img [src]="imageUrl" />
+        <img [src]=imageUrl />
     `
 })
 export class CoursesComponent {
     title = "List of courses";
-    courses;
-
-    // Dependency Injection: Injecting or providing dependencies of a class into its constructor
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses();
-    }
-
-    getTitle() {
-        return this.title;
-    }
-
-    // Logic for calling and HTTP service
-
+    imageUrl = "http://lorempixel.com/400/200/";
+    
 }
