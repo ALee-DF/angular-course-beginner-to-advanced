532bf5a3f165009770df7be9dd5eda70c30da6fa
Alexander Lee <alex8usa8@gmail.com>
2021-07-01 15:01:32 -0700

    Displaying Data and Handling Events: Two-way Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
+    // Messy two-way binding
+    // template: `
+    //     <input [value]="email" (keyup.enter)="email = $event.target.value; onKeyUp()" />
+    // `
+
     template: `
-        <input #email (keyup.enter)="onKeyUp(email.value)" />
+        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
     `
 })
 export class CoursesComponent {
+    email = 'me@example.com';
 
-    onKeyUp(email) {
-        console.log(email);
+    onKeyUp() {
+        console.log(this.email);
     }
 }
