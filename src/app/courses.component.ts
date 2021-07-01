73b57aa465b25bccc419cd2f3bca9f0be35b1c57
Alexander Lee <alex8usa8@gmail.com>
2021-07-01 14:50:11 -0700

    Displaying Data and Handling Events: Template Variables

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,27 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-
-    // Method 1
-    // template: `
-    //     <input (keyup)="onKeyUp($event)" />
-    // `
-
-    // Method 2
     template: `
-        <input (keyup.enter)="onKeyUp()" />
+        <input #email (keyup.enter)="onKeyUp(email.value)" />
     `
 })
 export class CoursesComponent {
-    // Method 1
-    // onKeyUp($event) {
-    //     if($event.keyCode === 13) {
-    //         console.log('ENTER was Pressed.')
-    //     }
-    // }
 
-    // Method 2 with Event Filtering
-    onKeyUp() {
-        console.log('ENTER was Pressed.')
+    onKeyUp(email) {
+        console.log(email);
     }
 }
