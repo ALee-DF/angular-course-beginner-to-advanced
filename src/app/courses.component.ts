dbcf4521f37a00fb27ba056d03b57c61d348e33b
Alexander Lee <31448956+ALee-DF@users.noreply.github.com>
2021-07-01 12:04:32 -0700

    Completed Angular fundamentals Section (#2)
    * Completed Angular Fundaments: Generating Components using Angular CLI
    
    * Angular Fundamentals: Templates
    
    * Angular Fundamentals: Directives
    
    * Angular Fundamentals: Services and Dependency Injection
    
    * Angular Fundamentals: Assignment 1 - List of Authors

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+
+    // Whenever you are using a directive that modifies the structure of the DOM by adding or removing an element,
+    // you need to prefix "ngFor" with an asterisks (*)
+    template: `
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
+    title = "List of courses";
+    courses;
+
+    // Dependency Injection: Injecting or providing dependencies of a class into its constructor
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    // Logic for calling and HTTP service
+
 }
