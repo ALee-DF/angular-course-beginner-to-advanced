4df4cace6f50c73b0b92524e29ea8176882e3ea6
Alexander Lee <alex8usa8@gmail.com>
2021-07-16 14:45:08 -0700

    Consuming HTTP Services: Extracting a Reusable Error Handling Method

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AppError } from '../common/app-error';
 import { BadInput } from '../common/bad-input';
@@ -17,32 +17,38 @@ export class PostService {
   }
 
   getPosts() {
-    return this.http.get(this.url);
+    return this.http.get(this.url).pipe(
+      catchError(this.handleError)
+    );
   }
 
   createPost(post) {
     return this.http.post(this.url, post).pipe(
-      catchError((error: HttpErrorResponse) => {
-        if (error.status === 400) {
-          return throwError(new BadInput(error));
-        }
-        return throwError(new AppError(error));
-      })
+      catchError(this.handleError)
     );
   }
 
   updatePost(post) {
-    return this.http.patch(`${this.url}/${post.id}`, { isRead: true });
+    return this.http.patch(`${this.url}/${post.id}`, { isRead: true }).pipe(
+      catchError(this.handleError)
+    );
   }
 
   deletePost(id: number) {
     return this.http.delete(`${this.url}/${id}`).pipe(
-      catchError((error: HttpErrorResponse) => {
-        if (error.status === 404) {
-          return throwError(new NotFoundError());
-        }
-        return throwError(new AppError(error));
-      })
+      catchError(this.handleError)
     );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 404) {
+      return throwError(new NotFoundError());
+    }
+
+    if (error.status === 400) {
+      return throwError(new BadInput(error));
+    }
+
+    return throwError(new AppError(error));
   }
 }
