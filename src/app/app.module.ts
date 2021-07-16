2f01d870410e821d362c5737b624f99f01a4c58f
Alexander Lee <31448956+ALee-DF@users.noreply.github.com>
2021-07-15 17:30:49 -0700

    Reactive forms (#7)
    * Reactive Forms: Building a Bootstrap Form
    
    * Reactive Forms: Creating Controls Programmatically
    
    * Reactive Forms: Adding Validation
    
    * Reactive Forms: Specific Validation Errors
    
    * Reactive Forms: Implementing Custom Validation
    
    * Reactive Forms: Asynchronous Operations and Asynchronous Validators
    
    * Reactive Forms: Showing a Loader Image
    
    * Reactive Forms: Validating the Form Input Upon Submit
    
    * Reactive Forms: Nested Form Groups
    
    * Reactive Forms: FormArray
    
    * Reactive Forms: FormBuilder
    
    * Reactive Forms: Assignment: Change Password Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
@@ -18,6 +18,10 @@ import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewCourseFormArrayComponent } from './new-course-form-array/new-course-form-array.component';
+import { NewCourseFormBuilderComponent } from './new-course-form-builder/new-course-form-builder.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   declarations: [
@@ -35,11 +39,16 @@ import { NewCourseFormComponent } from './new-course-form/new-course-form.compon
     InputFormatDirective,
     ZippyComponent,
     ContactFormComponent,
-    NewCourseFormComponent
+    NewCourseFormComponent,
+    SignupFormComponent,
+    NewCourseFormArrayComponent,
+    NewCourseFormBuilderComponent,
+    ChangePasswordComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     CoursesService,
