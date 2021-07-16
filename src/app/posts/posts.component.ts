d48be08c3086cb07f79b8ae5d4fb1d6b9e5c54df
Alexander Lee <alex8usa8@gmail.com>
2021-07-16 10:28:36 -0700

    Consuming HTTP Services: Creating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,13 +8,26 @@ import { Component } from '@angular/core';
 })
 export class PostsComponent {
   posts: any;
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
   // posts: any[]; doesn't work and results in a compilation error. Better to use interface instead.
 
-  constructor(http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  constructor(private http: HttpClient) {
+    http.get(this.url)
       .subscribe(response => {
         this.posts = response;
+      });
+  }
+
+  createPost(input:HTMLInputElement) {
+    const post = {
+      title: input.value
+    };
+    input.value = '';
+    this.http.post(this.url, post)
+      .subscribe(response => {
+        post['id'] = response['id'];
+        this.posts.splice(0, 0, post);
       });
   }
 }
